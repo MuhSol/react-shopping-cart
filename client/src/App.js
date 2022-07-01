f8064f3de8e30f332a6d05a5b95cbf326d2ab8a3
Mohamed Soliman <32719498+muhahm-creator@users.noreply.github.com>
2022-07-01 16:50:43 +0200

    Modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div className="layout">
+        <header>
+          <h2>Shopping Cart</h2>
+        </header>
+        <main>
+          <h2>Content</h2>
+        </main>
+        <footer>
+          <h2>Footer</h2>
+        </footer>
     </div>
   );
 }
